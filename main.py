6f56a30354ec5c01439eb2e316becaf3c02f49c2
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-05-23 18:50:27 -0400

    Key added temp key in config.json

--- main.py
+++ main.py
@@ -3,25 +3,38 @@ import flask
 from flask import request, jsonify
 from data.nsfw import nsfw_image
 import random
+import json
+
+with open('data/config.json') as config_file:
+    config = json.load(config_file)
+    token = config['token']
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
 @app.route('/', methods=['GET'])
 def home():
-    json_data = { "message": "Success", "version": 1 }
-    return jsonify(json_data)
+    headers = request.headers
+    auth = headers.get("X-Api-Key")
+    if auth == token:
+        return jsonify({"message": "OK: Authorized"}), 200
+    else:
+        return jsonify({"message": "You need to go to syntech.lol/api/v1/getkey", "error": "Unauthorized"}), 401
 
-@app.route(f'/api/v1', methods=['GET'])
+@app.route(f'/api/v1/', methods=['GET'])
 def api_v1():
     json_data = { "message": "No Endpoins Used" }
     return jsonify(json_data)
 
-@app.route(f'/api/v1/nsfw', methods=['GET'])
+@app.route(f'/api/v1/nsfw/', methods=['GET'])
 def api_v1_nsfw():
-    json_data = { "Endpoints": ["/api/v1/nsfw/<id>", "/api/v1/nsfw/random"], "message": "Success" }
-    return jsonify(json_data)
-
+    headers = request.headers
+    auth = headers.get("X-Api-Key")
+    if auth == token:
+        json_data = { "Endpoints": ["/api/v1/nsfw/<id>", "/api/v1/nsfw/random"], "message": "Success" }
+        return jsonify(json_data)
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
 
 @app.errorhandler(404)
 def page_not_found(e):
@@ -29,18 +42,28 @@ def page_not_found(e):
 
 @app.route(f'/api/v1/nsfw/<int:id>', methods=['GET'])
 def api_v1_nsfw_id(id):
-    if id > 3:
+    headers = request.headers
+    auth = headers.get("X-Api-Key")
+    if auth == token:
+     if id > 3:
         return jsonify({"message": "Not found"})
-    else:
+     else:
         find_image = [nsfw_image[id-1]]
         for i in find_image:
             json_data = { "message": "Success", "url": i['url'] }
             return jsonify(json_data)
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
 
 @app.route(f'/api/v1/nsfw/random', methods=['GET'])
 def api_v1_nsfw_random():
-    id = random.randint(1,3)
-    find_image = [nsfw_image[id-1]]
-    for i in find_image:
-        json_data = { "message": "Success", "url": i['url'] }
-        return jsonify(json_data)
+    headers = request.headers
+    auth = headers.get("X-Api-Key")
+    if auth == token:
+        id = random.randint(1,3)
+        find_image = [nsfw_image[id-1]]
+        for i in find_image:
+            json_data = { "message": "Success", "id": i['id'], "url": i['url'] }
+            return jsonify(json_data)
+    else:
+        return jsonify({"message": "ERROR: Unauthorized"}), 401
