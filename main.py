95de074f45227afb15d50ece9e51cec934ba4c65
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-05-23 20:40:29 -0400

    yes

--- main.py
+++ main.py
@@ -1,25 +1,37 @@
-from gettext import find
 import flask
 from flask import request, jsonify
+from requests import head
 from data.nsfw import nsfw_image
 import random
 import json
-
-with open('data/config.json') as config_file:
-    config = json.load(config_file)
-    token = config['token']
+import string
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+app.config['SECRET_KEY']='9nwof9uwfqoko99'
+
+@app.route('/api/v1/getkey', methods=['GET'])
+def get_token():
+    with open('data/config.json', 'w') as f:
+        token = ''.join(random.choices(string.ascii_letters + string.digits, k=32))
+        ip_addr = request.remote_addr
+        f.write(json.dumps({'ip_addr': ip_addr, 'token': token}))
+        f.close()
+    return jsonify({'token': token})
+
+@app.route('/client')
+def client():
+    ip_addr = request.environ['REMOTE_ADDR']
+    return '<h1> Your IP address is:' + ip_addr
 
 @app.route('/', methods=['GET'])
 def home():
-    headers = request.headers
-    auth = headers.get("X-Api-Key")
-    if auth == token:
-        return jsonify({"message": "OK: Authorized"}), 200
+    header = request.headers.get('Authorization')
+    if header == token:
+        return jsonify({'message': "Authorized"})
     else:
-        return jsonify({"message": "You need to go to syntech.lol/api/v1/getkey", "error": "Unauthorized"}), 401
+        return jsonify({'message': "Unauthorized"})
+
 
 @app.route(f'/api/v1/', methods=['GET'])
 def api_v1():
@@ -29,7 +41,7 @@ def api_v1():
 @app.route(f'/api/v1/nsfw/', methods=['GET'])
 def api_v1_nsfw():
     headers = request.headers
-    auth = headers.get("X-Api-Key")
+    auth = headers.get("Authorization")
     if auth == token:
         json_data = { "Endpoints": ["/api/v1/nsfw/<id>", "/api/v1/nsfw/random"], "message": "Success" }
         return jsonify(json_data)
@@ -43,7 +55,7 @@ def page_not_found(e):
 @app.route(f'/api/v1/nsfw/<int:id>', methods=['GET'])
 def api_v1_nsfw_id(id):
     headers = request.headers
-    auth = headers.get("X-Api-Key")
+    auth = headers.get("Authorization")
     if auth == token:
      if id > 3:
         return jsonify({"message": "Not found"})
@@ -58,7 +70,7 @@ def api_v1_nsfw_id(id):
 @app.route(f'/api/v1/nsfw/random', methods=['GET'])
 def api_v1_nsfw_random():
     headers = request.headers
-    auth = headers.get("X-Api-Key")
+    auth = headers.get("Authorization")
     if auth == token:
         id = random.randint(1,3)
         find_image = [nsfw_image[id-1]]
